b01ad98ab5dff4b7804f0a5f325ec9f382cedb2d
jonniek01 <jontejonhn@gmail.com>
2022-01-27 06:52:01 +0300

    portfolio prt 1

---
+++ js/portfolio.js
@@ -0,0 +1,58 @@
+var portfolios=[
+    {
+    id:1,
+    category:"health",
+    image:"../resources/cite.png",
+    title:"CITE",
+    link:"https://citeke.com/about.html"
+     },
+     {
+        id:1,
+        category:"health",
+        image:"../resources/cite.png",
+        title:"CITE",
+        link:"citeke.com"
+         },
+    {
+    id:1,
+    category:"health",
+    image:"../resources/cite.png",
+    title:"CITE",
+    link:"citeke.com"
+     },
+    {
+    id:1,
+    category:"health",
+    image:"../resources/cite.png",
+    title:"CITE",
+    link:"citeke.com"
+     }
+    
+]
+
+
+
+function renderAll(){
+    document.getElementById('portf').innerHTML = portfolios.map(portfolio => 
+        `
+        <div class="container" >
+        <div class="content">
+          <a href="${portfolio.link}" target="_blank">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="${portfolio.image}">
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">${portfolio.title}</h3>
+              <p class="content-text">This is a short description</p>
+            </div>
+          </a>
+        </div>
+      </div>
+
+        `
+
+        ).join('')
+}
+
+renderAll();
+
+
