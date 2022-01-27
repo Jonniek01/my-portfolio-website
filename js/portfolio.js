e2bd6c38fd3f0d3c288391f10b2b8026a283e47c
jonniek01 <jontejonhn@gmail.com>
2022-01-27 08:13:34 +0300

    portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,29 +5,27 @@ var portfolios=[
     image:"../resources/cite.png",
     title:"CITE",
     link:"https://citeke.com/about.html",
-    jid:"cite"
+    jid:"cite",
+    description:"WaziUp innovations 2021"
 
      },
-     {
-        id:1,
-        category:"health",
-        image:"../resources/cite.png",
-        title:"CITE",
-        link:"citeke.com"
-         },
     {
-    id:1,
-    category:"health",
-    image:"../resources/cite.png",
-    title:"CITE",
-    link:"citeke.com"
+    id:2,
+    category:"design",
+    image:"../resources/sos.png",
+    title:"SOSPETER",
+    link:"http://sospeter.afristarstech.comiteke.com/?i=1",
+        description:"A portfolio Website"
+
      },
     {
     id:1,
-    category:"health",
-    image:"../resources/cite.png",
-    title:"CITE",
-    link:"citeke.com",
+    category:"innovation",
+    image:"../resources/selonla.png",
+    title:"SELONLA",
+    link:"https://selonla.netlify.app",
+    description:"My project for Google solution challenge 2022"
+
      }
     
 ]
@@ -43,9 +41,11 @@ function renderAll(){
             <div class="content-overlay"></div>
             <img class="content-image" src="${portfolio.image}">
             <div class="content-details fadeIn-bottom">
-              <h3 class="content-title">${portfolio.title}</h3>
-              <p class="content-text">This is a short description</p>
+              <h3 class="content-title cor2" >${portfolio.title}</h3>
+              <p class="content-text">${portfolio.description}. <span class="cor2">Click to learn more<span></p>
+
             </div>
+
           </a>
         </div>
       </div>
